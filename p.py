818f1a40be302ea44c2e2b6b81e89fd5d07dff0c
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2023-09-25 21:29:49 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,19 +1,15 @@
 
-
-
-
-
-
 import streamlit as st
-st.header("المرحوم قويدر علاونه")
 
+st.header("المرحوم قويدر علاونه")
 #audio_url = "https://dl2.sura.pw/dl/reciter/1/32/001.mp3?h=tpbF6zaHPnBUmdu54vcwoQ&expires=1695739962&dl=true"
 audio_url='https://ia801400.us.archive.org/34/items/duaa-ommy_001/002.mp3'
+
 image_url = "https://j.top4top.io/p_2824pxkzj1.jpg"
 
 audio_html = f"""
     <audio autoplay controls style="display: none">
-        <source src="{audio_url}" type="audio/mp3">
+        <source src="{audio_url}" type="audio/mpeg">
         Your browser does not support the audio element.
     </audio>
     """
@@ -23,5 +19,3 @@ image_html = f"""
     <img src="{image_url}" style="width: 500px; height: auto">
     """
 st.markdown(image_html, unsafe_allow_html=True)
-
-
