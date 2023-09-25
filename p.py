82efc0d37b1233383775499e884b17bc33784d4b
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2023-09-25 21:33:36 +0300

    Update p.py

--- p.py
+++ p.py
@@ -6,8 +6,8 @@ st.markdown("""
         المرحوم قويدر علاونه
     </div>
 """, unsafe_allow_html=True)
-#audio_url = "https://dl2.sura.pw/dl/reciter/1/32/001.mp3?h=tpbF6zaHPnBUmdu54vcwoQ&expires=1695739962&dl=true"
-audio_url='https://ia801400.us.archive.org/34/items/duaa-ommy_001/002.mp3'
+audio_url = "https://dl2.sura.pw/dl/reciter/1/32/001.mp3?h=tpbF6zaHPnBUmdu54vcwoQ&expires=1695739962&dl=true"
+#audio_url='https://ia801400.us.archive.org/34/items/duaa-ommy_001/002.mp3'
 
 image_url = "https://j.top4top.io/p_2824pxkzj1.jpg"
 
