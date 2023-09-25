592367c342c5299043335af54ff8a0a2e34c484d
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2023-09-25 21:48:47 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 
 #audio_url = "https://dl2.sura.pw/dl/reciter/1/32/001.mp3?h=tpbF6zaHPnBUmdu54vcwoQ&expires=1695739962&dl=true"
@@ -19,8 +20,25 @@ audio_html = f"""
     </audio>
 """
 
+
+# Image URL
+image_url = "https://j.top4top.io/p_2824pxkzj1.jpg"
+
+# Image HTML
 image_html = f"""
-    <img src="{image_url}" style="width: 500px; height: auto">
-    """
+    <style>
+        @media only screen and (max-width: 600px) {{
+            img {{
+                width: 100%;
+            }}
+        }}
+        @media only screen and (min-width: 600px) {{
+            img {{
+                width: 50%;
+            }}
+        }}
+    </style>
+    <img src="{image_url}">
+"""
 st.markdown(audio_html, unsafe_allow_html=True)
 st.markdown(image_html, unsafe_allow_html=True)
