136a06b72e83a9dc63514aeda6305b0ca3a039cd
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2023-09-26 18:31:10 +0300

    Update alawneh.py

--- alawneh.py
+++ alawneh.py
@@ -1,6 +1,7 @@
 
 import streamlit as st
 import webbrowser
+import requests
 
 #audio_url = "https://dl2.sura.pw/dl/reciter/1/32/001.mp3?h=tpbF6zaHPnBUmdu54vcwoQ&expires=1695739962&dl=true"
 image_url = "https://j.top4top.io/p_2824pxkzj1.jpg"
